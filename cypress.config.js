7f0c2678886800bd665e4f7e44a2c4aea64e34a0
mikamttr <mika348@live.fr>
2024-06-21 11:41:32 +0200

    v0.0

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
   e2e: {
+    excludeSpecPattern: '**/examples/*.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,11 +1,11 @@
-// /// <reference types="cypress" />
+/// <reference types="cypress" />
 
-// context('Hello World!', () => {
-//     beforeEach(() => {
-//         cy.visit('../../app/index.html');
-//     });
-//     it('p should have text Hello World!', () => {
-//         cy.get('p')
-//             .should('have.text', 'Hello World!');
-//     });
-// })
+context('Hello World!', () => {
+    beforeEach(() => {
+        cy.visit('../../app/index.html');
+    });
+    it('p should have text Hello World!', () => {
+        cy.get('h1')
+            .should('have.text', 'Hello World');
+    });
+})
