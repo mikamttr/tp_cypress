2917330dc3cc65687fb13a0e22976cf9a15f969d
mikamttr <mika348@live.fr>
2024-06-21 13:53:10 +0200

    v0.1

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -4,8 +4,18 @@ context('Hello World!', () => {
     beforeEach(() => {
         cy.visit('../../app/index.html');
     });
-    it('p should have text Hello World!', () => {
+    it('h1 should have text Hello World', () => {
         cy.get('h1')
             .should('have.text', 'Hello World');
+    });
+})
+
+context('Hello World!', () => {
+    beforeEach(() => {
+        cy.visit('../../app/index.html');
+    });
+    it('p should have text Coucou', () => {
+        cy.get('p')
+            .should('have.text', 'Coucou');
     });
 })
